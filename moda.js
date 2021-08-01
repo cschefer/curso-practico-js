8ce1fecafafd8393da6df008d3a8baf2e5404fee
Christian A. Schefer <cschefer@outlook.com>
2021-08-01 19:55:53 -0300

    cambios en moda.js convirtiendola en funcion

--- moda.js
+++ moda.js
@@ -1,19 +1,64 @@
-const lista1 = [1, 2, 3, 1, 2, 3, 4, 2, 2, 2, 1];
-
 const lista1Count = {};
 
-lista1.map(function (elemento) {
-  if (lista1Count[elemento]) {
-    lista1Count[elemento] += 1;
-  } else {
-    lista1Count[elemento] = 1;
-  }
-});
+function calcularModa(lista) {
+  lista.map(function (elemento) {
+    if (lista1Count[elemento]) {
+      lista1Count[elemento] += 1;
+    } else {
+      lista1Count[elemento] = 1;
+    }
+  });
 
-const lista1Array = Object.entries(lista1Count).sort(
-  function (a, b) {
-    return a[1] - b[1];
+  const lista1Array = Object.entries(lista1Count).sort(
+    function (a, b) {
+      return a[1] - b[1];
+    }
+  );
+
+  const moda = lista1Array[lista1Array.length - 1];
+  return moda;
+}
+
+function onClickButtonAddValue(val) {
+  if (val != false) {
+    // agregarlo al array
+    val = Number(val);
+    lista.push(val);
+    lista.sort(compareNumbers);
+    // recargar listado html
+    addtoHtmlList();
+    // calcular la mediana
+    calcularModa(lista);
+
+    // limpia input
+    input = document.getElementById('inputValue');
+    input.value = '';
   }
+}
+
+const htmlList = document.getElementById('arrayValues');
+const medianaHtml = document.getElementById(
+  'medianaCalculada'
 );
+// function addtoHtmlList() {
+// for (var i = 0; i < lista.length; i++) {
+//   var el = '<li>' + lista[i] + '</li>';
+//   htmlList.insertAdjacentHTML('beforeend', el);
+// }}
 
-const moda = lista1Array[lista1Array.length - 1];
+function addtoHtmlList() {
+  htmlList.innerHTML = '';
+  for (var i = 0; i < lista.length; i++) {
+    var value = lista[i];
+    var el = '<li>' + value + '</li>';
+    htmlList.insertAdjacentHTML('beforeend', el);
+  }
+}
+
+function onClickButtonVaciarArray() {
+  // vacia el array
+  lista = [];
+  // vacia el listado html
+  htmlList.innerHTML = '';
+  medianaHtml.innerText = '';
+}
